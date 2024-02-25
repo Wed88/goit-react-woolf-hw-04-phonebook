6175430f47ac97ddfda35dcf438a1988972b5eb6
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2024-02-25 02:54:22 +0200

    додає рефакторінг коду

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,70 +1,70 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import shortid from 'shortid';
 import { Form, Label, Input, ButtonSubmit } from './ContactForm.styled';
 
-export default class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export default function ContactForm({ onSubmitContact }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const nameImputId = shortid.generate();
+  const numberImputId = shortid.generate();
+
+  const hendleImputChange = event => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  nameImputId = shortid.generate();
-  numberImputId = shortid.generate();
-
-  hendleImputChange = event => {
-    const { name, value } = event.currentTarget;
-
-    this.setState({ [name]: value });
-  };
-
-  hendleSubmit = event => {
+  const hendleSubmit = event => {
     event.preventDefault();
 
-    const { name, number } = this.state;
     const newContact = {
       id: shortid.generate(),
       name: name,
       number: number,
     };
 
-    this.props.onSubmitContact(newContact);
+    onSubmitContact(newContact);
 
-    this.reset();
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { hendleSubmit, nameImputId, hendleImputChange, numberImputId } =
-      this;
-    const { name, number } = this.state;
-
-    return (
-      <Form onSubmit={hendleSubmit}>
-        <Label htmlFor={nameImputId}>Name</Label>
-        <Input
-          type="text"
-          name="name"
-          value={name}
-          onChange={hendleImputChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <Label htmlFor={numberImputId}>Number</Label>
-        <Input
-          type="tel"
-          name="number"
-          value={number}
-          onChange={hendleImputChange}
-          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-        <ButtonSubmit type="submit">Add contact</ButtonSubmit>
-      </Form>
-    );
-  }
+  return (
+    <Form onSubmit={hendleSubmit}>
+      <Label htmlFor={nameImputId}>Name</Label>
+      <Input
+        type="text"
+        name="name"
+        value={name}
+        onChange={hendleImputChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <Label htmlFor={numberImputId}>Number</Label>
+      <Input
+        type="tel"
+        name="number"
+        value={number}
+        onChange={hendleImputChange}
+        pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <ButtonSubmit type="submit">Add contact</ButtonSubmit>
+    </Form>
+  );
 }
